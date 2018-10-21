ff115a577431dd40076a4f188fb9acd2ea09a30e
zhangxinyao88 <43081360+zhangxinyao88@users.noreply.github.com>
2018-10-21 00:37:29 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -5,28 +5,42 @@ import time
 #from selenium.webdriver.common.by import By
 #from selenium.webdriver.common.keys import Keys
 
+pages_array = []
+jobs_array = []
 theJob = "software+engineer"
-the_url = "https://www.indeed.com/jobs?q=" + theJob + "&l="
-browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
-
-browser.set_page_load_timeout("5")
-browser.get(the_url)
-#browser.find_element_by_id("sja1").click()
-
-Jobs = browser.find_elements_by_xpath("//*[contains(@class,'row result clickcard')]")
-for items in Jobs:
-    job = items.find_element_by_class_name('snip') # tip: class="sjCapt" class = "iaP"
-    description = job.text
-    if description.find("Easily apply") != -1:
-        job_title = items.find_element_by_tag_name("a").text
-        company = items.find_element_by_class_name("company").text
-        location = items.find_element_by_class_name("location").text
-        #print(job_title + "\n" + company + "\n" + location)
-        a = items.find_element_by_xpath(".//a")
-        url = a.get_attribute("href")
-        print(url)
+home_page = "https://www.indeed.com/jobs?q=" + theJob + "&l="
 
 
+def get_web_driver(the_url):
+    browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
+    browser.set_page_load_timeout("5")
+    browser.get(the_url)
+    return browser
 
-time.sleep(5)
-#browser.quit()
+
+def close_web_driver(browser):
+    time.sleep(5)
+    browser.quit()
+
+
+def page_parse(the_url):
+
+    # browser.find_element_by_id("sja1").click()
+    browser = get_web_driver(the_url)
+    jobs = browser.find_elements_by_xpath("//*[contains(@class,'row result clickcard')]") # go through each box
+
+    for items in jobs:
+        job = items.find_element_by_class_name('snip') # tip: class="sjCapt" class = "iaP"
+        description = job.text
+        if description.find("Easily apply") != -1:
+            job_title = items.find_element_by_tag_name("a").text
+            company = items.find_element_by_class_name("company").text
+            location = items.find_element_by_class_name("location").text
+            #print(job_title + "\n" + company + "\n" + location)
+            a = items.find_element_by_xpath(".//a")
+            url = a.get_attribute("href")
+            jobs_array.append(url)
+            print(url)
+
+
+page_parse(home_page)
