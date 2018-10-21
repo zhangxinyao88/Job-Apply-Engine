356cd1576011cd21da7015f3d572fbbd730042d7
zhangxinyao88 <43081360+zhangxinyao88@users.noreply.github.com>
2018-10-20 22:53:50 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -5,23 +5,28 @@ import time
 #from selenium.webdriver.common.by import By
 #from selenium.webdriver.common.keys import Keys
 
-theJob = "software+engineer+intern"
-url = "https://www.indeed.com/jobs?q=" + theJob + "&l="
+theJob = "software+engineer"
+the_url = "https://www.indeed.com/jobs?q=" + theJob + "&l="
 browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe')
 
 browser.set_page_load_timeout("5")
-browser.get(url)
+browser.get(the_url)
 #browser.find_element_by_id("sja1").click()
 
 Jobs = browser.find_elements_by_xpath("//*[contains(@class,'row result clickcard')]")
 for items in Jobs:
-    job = items.find_element_by_class_name('snip')
+    job = items.find_element_by_class_name('snip') # tip: class="sjCapt" class = "iaP"
     description = job.text
     if description.find("Easily apply") != -1:
         job_title = items.find_element_by_tag_name("a").text
         company = items.find_element_by_class_name("company").text
         location = items.find_element_by_class_name("location").text
-        print(job_title + "\n" + company + "\n" + location)
+        #print(job_title + "\n" + company + "\n" + location)
+        a = items.find_element_by_xpath(".//a")
+        url = a.get_attribute("href")
+        print(url)
+
+
 
 time.sleep(5)
 #browser.quit()
